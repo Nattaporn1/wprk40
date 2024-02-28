07a73cdb75729052b95f5d8263a56aae7946b3ad
Nattaporn Kanjanarujeerat <154008042+Nattaporn1@users.noreply.github.com>
2024-02-28 13:50:55 +0700

    Update ex3.java

--- ex3.java
+++ ex3.java
@@ -19,3 +19,13 @@ public class Teacher {
         System.out.println("Teaching");
     }
 }
+ class JavaExample extends Teacher{
+    String mainSubject = "Physics";
+    public static void main(String[] args) {
+        JavaExample obj = new JavaExample();
+        System.out.println(obj.getCollegeName());
+        System.out.println(obj.getDesignation());
+        System.out.println(obj.mainSubject);
+        obj.does();
+    }
+}
